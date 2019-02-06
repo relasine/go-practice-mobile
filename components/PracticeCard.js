b5e8fb7961174817690c646586662f9791dc987e
Kevin Simpson <relasine@gmail.com>
2019-02-06 11:36:17 -0700

    Write initial practice card logic

--- components/PracticeCard.js
+++ components/PracticeCard.js
@@ -1,26 +1,121 @@
 import React, { Component } from "react";
-
 import {
-  Stylesheet,
+  StyleSheet,
   View,
-  Scrollview,
+  ScrollView,
   Text,
   TextInput,
   Button
 } from "react-native";
 
+import NewSection from "./NewSection";
+import Section from "./Section";
+
 export default class PracticeCard extends Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      sections: [
+        {
+          id: 1549427088449,
+          piece: "#42 Jim Along Josie",
+          section: "First half",
+          skill: "Eight Note Rhythm",
+          time: "12"
+        }
+      ],
+      goals: ""
+    };
   }
 
+  handleGoalPress = goals => {
+    this.setState({ goals });
+  };
+
+  addNewSection = section => {
+    this.setState({
+      sections: [...this.state.sections, section]
+    });
+  };
+
+  deleteSection = id => {
+    const newSections = this.state.sections.filter(section => {
+      return section.id !== id;
+    });
+    this.setState({
+      sections: newSections
+    });
+  };
+
   render() {
+    const totalTime = this.state.sections.reduce((sum, section) => {
+      return sum + parseInt(section.time);
+    }, 0);
+
+    const practiceSections = this.state.sections.map((section, index) => {
+      return (
+        <Section
+          data={section}
+          deleteSection={this.deleteSection}
+          key={`section #${index + 1}`}
+        />
+      );
+    });
+
     return (
-      <ScrollView>
-        <View />
+      <ScrollView style={styles.container}>
+        <View style={styles.practiceCardTop}>
+          <Text style={styles.headerText}>New Practice Record</Text>
+          <Text style={styles.text}>Total Time - {totalTime} minutes</Text>
+          <TextInput
+            placeholderTextColor="#8995b7"
+            onChangeText={text => this.handleGoalPress(text)}
+            style={styles.input}
+            placeholder="Practice session goals"
+            value={this.state.goals}
+          />
+          <Button onPress={() => {}} title="Submit Practice Session" />
+        </View>
+        <NewSection addNewSection={this.addNewSection} />
+        <Text style={[styles.text, styles.sectionHeader]}>Songs Practiced</Text>
+        {practiceSections}
       </ScrollView>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignSelf: "stretch"
+  },
+  headerText: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 30,
+    textAlign: "center",
+    fontFamily: "Malayalam Sangam MN",
+    color: "#d5d7de"
+  },
+  practiceCardTop: {
+    padding: 16
+  },
+  input: {
+    marginTop: 16,
+    marginBottom: 16,
+    fontSize: 20
+  },
+  text: {
+    fontSize: 20,
+    fontFamily: "Malayalam Sangam MN",
+    color: "#8995b7",
+    textAlign: "center"
+  },
+  sectionHeader: {
+    textAlign: "center",
+    fontWeight: "bold",
+    paddingTop: 16,
+    color: "#d5d7de"
+  }
+});
