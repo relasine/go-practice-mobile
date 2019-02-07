d4d7ce378fcc4e5ebed2402ffcf06c1d34532f79
Kevin Simpson <relasine@gmail.com>
2019-02-06 18:53:35 -0700

    Create and import PracticeRecord component

---
+++ components/PracticeRecord.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import { View, Image, Text, StyleSheet } from "react-native";
+
+export default class PracticeRecord extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {},
+  text: {}
+});
--- components/Records.js
+++ components/Records.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { View, StyleSheet, ScrollView, Text, Image } from "react-native";
 
+import PracticeRecord from "./PracticeRecord";
+
 export default class Records extends Component {
   render() {
     return (
