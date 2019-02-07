9f05714f4091c499c788aaf8cfc7b593cf27bfda
Kevin Simpson <relasine@gmail.com>
2019-02-07 15:57:22 -0700

    Add PracticeCard posting

--- utilities/fetchCalls.js
+++ utilities/fetchCalls.js
@@ -23,3 +23,21 @@ export const fetchStudentData = async id => {
 
   return await response.json();
 };
+
+export const addPracticeCard = async (payload, id) => {
+  const requestBody = {
+    method: "POST",
+    mode: "cors",
+    body: JSON.stringify(payload),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  };
+
+  const response = await fetch(
+    `http://localhost:3000/api/v1/students/${id}/practice`,
+    requestBody
+  );
+
+  return await response.json();
+};
