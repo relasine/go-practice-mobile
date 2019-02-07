12bddac0621986c9cf444046d4950208b4550e5b
Kevin Simpson <relasine@gmail.com>
2019-02-06 18:49:37 -0700

    Import Records, Settings, and Nav into Main

--- components/Main.js
+++ components/Main.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import { StyleSheet, View, Text } from "react-native";
 
 import PracticeCard from "./PracticeCard";
+import Settings from "./Settings";
+import Records from "./Records";
+import Nav from "./Nav";
 
 export default class Main extends Component {
   constructor() {
@@ -12,10 +15,21 @@ export default class Main extends Component {
     };
   }
 
+  navigate = currentPage => {
+    if (currentPage !== this.state.currentPage) {
+      this.setState({
+        currentPage
+      });
+    }
+  };
+
   render() {
     return (
       <View style={styles.container}>
+        {this.state.currentPage === "records" && <Records />}
         {this.state.currentPage === "practice card" && <PracticeCard />}
+        {this.state.currentPage === "settings" && <Settings />}
+        <Nav navigate={this.navigate} />
       </View>
     );
   }
