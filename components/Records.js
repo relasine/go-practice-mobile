85d7e191f1314e42a7f7633847c858915998ea92
Kevin Simpson <relasine@gmail.com>
2019-02-07 15:57:05 -0700

    Record styling and logic

--- components/Records.js
+++ components/Records.js
@@ -5,14 +5,29 @@ import PracticeRecord from "./PracticeRecord";
 
 export default class Records extends Component {
   render() {
+    const practiceRecords = this.props.user.sessions.map(session => {
+      return <PracticeRecord key={session.id} data={session} />;
+    });
+
     return (
       <ScrollView style={styles.container}>
-        <View />
+        <Text style={styles.headerText}>Practice Records</Text>
+        {practiceRecords}
       </ScrollView>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {}
+  container: {
+    padding: 16
+  },
+  headerText: {
+    fontSize: 25,
+    fontWeight: "bold",
+    marginBottom: 30,
+    textAlign: "center",
+    fontFamily: "Malayalam Sangam MN",
+    color: "#d5d7de"
+  }
 });
