f12e0f674462458b2c31661dec397b626f0c8172
Kevin Simpson <relasine@gmail.com>
2019-02-13 16:05:22 -0700

    Add Error card modal

---
+++ components/ErrorCard.js
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+import { Modal, View, Text, TouchableOpacity, StyleSheet } from "react-native";
+
+export default class ErrorCard extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Modal
+          style={styles.modal}
+          animationType="slide"
+          transparent={false}
+          visible={this.props.modalVisible}
+        >
+          <View
+            style={{
+              backgroundColor: "#2c3753",
+              height: "100%",
+              alignItems: "center",
+              justifyContent: "center"
+            }}
+          >
+            <Text style={styles.text}>
+              Server error. Please try again later...
+            </Text>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={this.props.closeModal}
+            >
+              <Text style={styles.submit}>Back</Text>
+            </TouchableOpacity>
+          </View>
+        </Modal>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {},
+  modal: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  text: {
+    marginTop: 100,
+    marginBottom: 16,
+    textAlign: "center",
+    color: "#d5d5d5"
+  },
+  button: {
+    backgroundColor: "#333",
+    width: 100,
+    paddingBottom: 4,
+    borderRadius: 20,
+    alignSelf: "center",
+    marginTop: 0,
+    marginBottom: 32
+  },
+  submit: {
+    fontFamily: "Malayalam Sangam MN",
+    fontSize: 18,
+    textAlign: "center",
+    color: "#d5d5d5",
+    fontWeight: "bold",
+    paddingTop: 8
+  }
+});
