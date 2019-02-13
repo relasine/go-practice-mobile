b34fcf9ead89dc512bc9c5a851ba89ce41350c22
Kevin Simpson <relasine@gmail.com>
2019-02-13 16:04:55 -0700

    Restyle ChangePassword

--- components/ChangePassword.js
+++ components/ChangePassword.js
@@ -149,7 +149,7 @@ const styles = StyleSheet.create({
     paddingLeft: 8
   },
   button: {
-    backgroundColor: "#8995b7",
+    backgroundColor: "#333",
     width: 100,
     paddingBottom: 4,
     borderRadius: 20,
@@ -161,7 +161,7 @@ const styles = StyleSheet.create({
     fontFamily: "Malayalam Sangam MN",
     fontSize: 18,
     textAlign: "center",
-    color: "#d5d7de",
+    color: "#d5d5d5",
     fontWeight: "bold",
     paddingTop: 8
   }
