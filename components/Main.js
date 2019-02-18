0cde6c8a3b458f4b580ee731139931f109671046
Kevin Simpson <relasine@gmail.com>
2019-02-18 12:44:34 -0700

    Add logout as props to Main

--- components/Main.js
+++ components/Main.js
@@ -27,16 +27,27 @@ export default class Main extends Component {
     return (
       <View style={styles.container}>
         {this.state.currentPage === "records" && this.props.user && (
-          <Records user={this.props.user} updateUser={this.props.updateUser} />
+          <Records
+            user={this.props.user}
+            updateUser={this.props.updateUser}
+            webtoken={this.props.user.student.webtoken}
+            logout={this.props.logout}
+          />
         )}
         {this.state.currentPage === "practice card" && (
           <PracticeCard
             updateUser={this.props.updateUser}
             userId={this.props.user.student.id}
+            webtoken={this.props.user.student.webtoken}
+            logout={this.props.logout}
           />
         )}
         {this.state.currentPage === "settings" && (
-          <Settings updateUser={this.props.updateUser} user={this.props.user} />
+          <Settings
+            updateUser={this.props.updateUser}
+            user={this.props.user}
+            logout={this.props.logout}
+          />
         )}
         <Nav navigate={this.navigate} />
       </View>
